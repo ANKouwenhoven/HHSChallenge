b1eed80a2748049c572fe03465bb0700464ee413
aron <a-dingemanse-1@student.hhs.nl>
2022-11-21 15:02:14 +0100

    add basic framework for managing pages (not complete)

---
+++ src/com/codebind/Main.java
@@ -0,0 +1,14 @@
+package com.codebind;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] Args) {
+        ContentManager contentManager = new ContentManager();
+    }
+
+
+
+}
