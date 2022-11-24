f72a6b9c1254b7442ae04018b4c5eae36f957ed8
Merlijn <22117814@student.hhs.nl>
2022-11-24 12:28:33 +0100

    normale body verwisseld voor getBody, - regel 64 tot 87 toegevoegd met: - graph_over_time knop - login knop - aboutUs label //voor text en afbeeldingen waarin wij ons bedrijf voorstellen
    normale body verwisseld voor getBody,
    - panel met text "text" toegevoegd
    - knop van mockups toegevoegd

--- src/com/codebind/ContentManager.java
+++ src/com/codebind/ContentManager.java
@@ -2,6 +2,10 @@ package com.codebind;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+
+import static java.awt.Color.blue;
+import static java.awt.SystemColor.text;
 
 public class ContentManager {
 
@@ -9,13 +13,14 @@ public class ContentManager {
     JFrame frame;
 
 
-    JPanel body = new JPanel();
+
     // ??? pageHistory;
 
     // {{ init
     public ContentManager() {
         this.frame = getBasicFrame();
         this.frame.setVisible(true);
+        this.frame.setSize(1600,960);
     }
 
 
@@ -25,7 +30,7 @@ public class ContentManager {
         pane.setLayout(new BorderLayout());
 
         pane.add(this.getHeader(),"North");
-        pane.add(this.body, "South");
+        pane.add(this.getBody(),"Center");
         return frame;
     }
 
@@ -39,5 +44,50 @@ public class ContentManager {
         return panel;
     }
 
+    private JPanel getBody() {
+        JPanel panel = new JPanel();
+        //panel.setBackground(new Color(0xDAF7A6));
+        panel.setName("MAIN_BODY");
+        this.titleLabel = new JLabel("Text");
+        panel.add(this.titleLabel);
+
+
+        JButton mockups = new JButton();
+        //panel.setBackground(new Color(0xABBEC3A));
+        panel.setName("MOCKUP_KNOP");
+        mockups.setPreferredSize(new Dimension(100, 60));
+        this.titleLabel = new JLabel("Mockups");
+        panel.add(this.titleLabel);
+        panel.add(mockups);
+
+
+        JButton graph_over_time = new JButton();
+        //panel.setBackground(new Color(0XABBEC3B));
+        panel.setName("GRAPH_OVER_TIME");
+        graph_over_time.setPreferredSize(new Dimension(100,60));
+        this.titleLabel = new JLabel("GraphOverTime");
+        panel.add(this.titleLabel);
+        panel.add(graph_over_time);
+
+
+        JButton login = new JButton();
+        panel.setBackground(new Color(0X783FFFE8));
+        panel.setName("LOGIN");
+        login.setPreferredSize(new Dimension(60,60));
+        this.titleLabel = new JLabel("Login");
+        panel.add(this.titleLabel);
+        panel.add(login);
+
+
+        JLabel aboutUs = new JLabel();
+        panel.setName("ABOUT_US");
+        aboutUs.setPreferredSize(new Dimension(1600,400));
+        this.titleLabel = new JLabel("About us: test.test.test.test.test.test.test.test.test."); //positie aanpassen
+        panel.add(this.titleLabel);
+        panel.add(aboutUs);
+
+
+        return panel;
+    }
     // {{ body logic
 }
